f02cfafc7b2608c1464fd9b004111e615947dcdf
Mayank Panchal <mayank@Mayanks-MacBook-Pro.local>
2025-04-15 22:53:35 +0530

    Updates

---
+++ precache-manifest.c32732b58d8b9572832f5d959efc393d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6215bfd990ee5c871328c5c40ed0bdcd",
+    "url": "/Memory_match_cards/index.html"
+  },
+  {
+    "revision": "10838ac3a62a8b1395d2",
+    "url": "/Memory_match_cards/static/css/main.f63de3a5.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/Memory_match_cards/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "10838ac3a62a8b1395d2",
+    "url": "/Memory_match_cards/static/js/main.eccecc7a.chunk.js"
+  },
+  {
+    "revision": "dfe08751f0ece305f082",
+    "url": "/Memory_match_cards/static/js/runtime~main.ff8fc0ff.js"
+  }
+]);
